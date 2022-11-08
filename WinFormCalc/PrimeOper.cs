0669461c6ea34b0d0a9db74e1c2dc34bd44831f8
Nitram293 <kratochvilm.04@spst.eu>
2022-11-08 14:32:18 +0100

    Create PrimeOper

---
+++ WinFormCalc/PrimeOper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormCalc
+{
+    public enum PrimeOper
+    {
+        None,
+        Multiply,
+        Divide,
+        Power
+    }
+}
